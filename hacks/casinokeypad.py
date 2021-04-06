8431af1a5d4d7d309410d3c4e5a7b7d2defeda43
JU5TDIE <justdie4444@gmail.com>
2021-04-07 03:10:41 +0900

    dot_check function - casinokeypad.py
    Add pixel_check function
    
    Arguments : a, img
    
    Crop image(img argument) to pixel with a argument (length).
    
    If the pixel is completely white, append 1 in list(hint). Else append 0. (do this 5 times)
    
    Put list(hint) as key in dictionary(DIGITS_LOOKUP) and then return value from it

--- hacks/casinokeypad.py
+++ hacks/casinokeypad.py
@@ -17,3 +17,16 @@ height = [2, 110, 218, 326, 434]
 length = [50, 158, 266, 374, 482, 590]
 
 bbox = (454, 300, 1080, 830)
+
+def dot_check(a, img):
+    hint = []
+
+    for i in range(0, 5):
+        crop_img = img[height[i]:height[i] + 1, length[a]:length[a] + 1]
+
+        if np.mean(crop_img) == 255:
+            hint.append(1)
+        else:
+            hint.append(0)
+
+    return DIGITS_LOOKUP[tuple(hint)]
