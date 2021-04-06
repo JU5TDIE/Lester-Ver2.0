8ade78de8b28e062bb5d662c89a474f707382476
JU5TDIE <justdie4444@gmail.com>
2021-04-07 02:42:14 +0900

    DIGITS_LOOKUP dictionary - casinokeypad.py

--- hacks/casinokeypad.py
+++ hacks/casinokeypad.py
@@ -4,3 +4,11 @@ import time
 import keyboard
 import numpy as np
 from PIL import ImageGrab
+
+DIGITS_LOOKUP = {
+    (1, 0, 0, 0, 0): 1,
+	(0, 1, 0, 0, 0): 2,
+	(0, 0, 1, 0, 0): 3,
+	(0, 0, 0, 1, 0): 4,
+	(0, 0, 0, 0, 1): 5
+}
