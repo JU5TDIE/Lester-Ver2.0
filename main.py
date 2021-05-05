733379c087fbdf73d0d57a3c7a1345f5c9aacde4
JU5TDIE <justdie4444@gmail.com>
2021-05-05 17:29:25 +0900

    check_window function - main.py
    if Grand Theft Auto V is detected, return True

--- main.py
+++ main.py
@@ -3,3 +3,17 @@ import pynput
 import win32gui
 from threading import Thread
 from hacks import casinofingerprint, casinokeypad, cayofingerprint, cayovoltage
+
+def check_window():
+	print('[*] Searching Grand Theft Auto V...')
+
+	while True:
+		hwnd = win32gui.FindWindow(None, "Grand Theft Auto V")
+		
+		if hwnd:
+			print('[*] Grand Theft Auto V Detected!')
+			print('')
+			print('')
+			return True
+		
+		time.sleep(1)
