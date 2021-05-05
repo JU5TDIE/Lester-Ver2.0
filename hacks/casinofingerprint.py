230562d48c8371c8bb2f6367c686d1486a448186
JU5TDIE <justdie4444@gmail.com>
2021-05-05 17:09:31 +0900

    casinofingerprint.py, cayofingerprint.py
    https://github.com/RedHeadEmile/GTA-V-Heist-Help, copied from this repository

---
+++ hacks/casinofingerprint.py
@@ -0,0 +1,71 @@
+import cv2
+import time
+import keyboard
+import numpy as np
+from PIL import Image, ImageGrab
+
+bbox = (0, 0, 1920, 1080)
+
+tofind = (950, 155, 1335, 685)
+
+parts = [[(482, 279, 482 + 102, 279 + 102), (0, 0)],
+[(627, 279, 627 + 102, 279 + 102), (1, 0)],
+[(482, 423, 482 + 102, 423 + 102), (0, 1)],
+[(627, 423, 627 + 102, 423 + 102), (1, 1)],
+[(482, 566, 482 + 102, 566 + 102), (0, 2)],
+[(627, 566, 627 + 102, 566 + 102), (1, 2)],
+[(482, 711, 482 + 102, 711 + 102), (0, 3)],
+[(627, 711, 627 + 102, 711 + 102), (1, 3)]]
+
+def is_in(img, subimg):
+	"""return if 'subimg' is in 'img'"""
+	subimg1 = cv2.cvtColor(np.array(subimg), cv2.COLOR_BGR2GRAY) # need gray image to do the matchTemplate
+	res = cv2.matchTemplate(img, subimg1, cv2.TM_CCOEFF_NORMED)
+	threshold = 0.65 # error coef
+	loc = np.where(res >= threshold)
+	for pt in zip(*loc[::-1]):
+		return True
+	return False
+
+def main():
+	print('[*] Casino Fingerprint')
+	im = ImageGrab.grab(bbox)
+
+	sub0_ = im.crop(tofind)
+	sub0 = cv2.cvtColor(np.array(sub0_.resize((round(sub0_.size[0] * 0.77), round(sub0_.size[1] * 0.77)))), cv2.COLOR_BGR2GRAY) # need to resize the image because fingerprints parts is smaller than the image + need gray image to do the matchTemplate
+
+	# will store the location of the rights fingerprints
+	togo = [part[1] for part in parts if is_in(sub0, im.crop(part[0]))]
+
+	# closing every images
+	sub0_.close()
+	im.close()
+
+	moves = []
+
+	x, y = 0, 0
+	for pos in togo:
+		while x != pos[0]:
+			if x > pos[0]:
+				x -= 1
+				moves.append("a")
+			else:
+				x += 1
+				moves.append("d")
+		
+		while y != pos[1]:
+			if y > pos[1]:
+				y -= 1
+				moves.append("w")
+			else:
+				y += 1
+				moves.append("s")
+		moves.append("return")
+	moves.append("tab")
+
+	print('-', moves)
+	for key in moves:
+		keyboard.press_and_release(key)
+		time.sleep(0.025)
+	print('[*] END')
+	print('=============================================')
---
+++ hacks/cayofingerprint.py
@@ -0,0 +1,70 @@
+import cv2
+import time
+import keyboard
+import numpy as np
+from PIL import Image, ImageGrab
+
+bbox = (0, 0, 1920, 1080)
+
+targets = [(907, 331, 1562, 431), # split the big digit in 8 parts
+(907, 404, 1562, 504),
+(907, 500, 1562, 600),
+(907, 560, 1562, 660),
+(907, 627, 1562, 727),
+(907, 697, 1562, 809),
+(907, 780, 1562, 883),
+(907, 863, 1562, 975)]
+
+scan = [(424, 360, 810, 415), # every parts on the left
+(424, 360 + 76, 810, 415 + 76),
+(424, 360 + 76 * 2, 810, 415 + 76 * 2),
+(424, 360 + 76 * 3, 810, 415 + 76 * 3),
+(424, 360 + 76 * 4, 810, 415 + 76 * 4),
+(424, 360 + 76 * 5, 810, 415 + 76 * 5),
+(424, 360 + 76 * 6, 810, 415 + 76 * 6),
+(424, 360 + 76 * 7, 810, 415 + 76 * 7)]
+
+
+def index(part, parts):
+	"""Return the index of 'part' in 'parts', return -1 if not found"""
+	for i in range(len(parts)):
+		res = cv2.matchTemplate(parts[i], part, cv2.TM_CCOEFF_NORMED)
+		loc = np.where(res >= 0.65)
+		for pt in zip(*loc[::-1]):
+			return i
+	return -1
+
+def main():
+	print('[*] Cayo Perico Fingerprint')
+	im = ImageGrab.grab(bbox)
+
+	parts = []
+	for target in targets:
+		part = im.crop(target)
+		# resize the big part and store it
+		parts.append(cv2.cvtColor(np.array(part.resize((round(part.size[0] * 0.91), round(part.size[1] * 0.91)))), cv2.COLOR_BGR2GRAY))
+
+	moves = []
+	for i in range(len(scan)):
+		# get the index of the litle part on  the left
+		j = index(cv2.cvtColor(np.array(im.crop(scan[i])), cv2.COLOR_BGR2GRAY), parts)
+
+		path = min(i - j, i - j - 8, i - j + 8, key = abs)
+		if path != 0:
+			key = "d" if path > 0 else "a"
+			for i in range(abs(path)):
+				moves.append(key)
+
+		moves.append("s")
+
+	while moves[-1] == "S":
+		del moves[-1]
+
+	print('-', moves)
+	
+	for key in moves:
+		keyboard.press_and_release(key)
+		time.sleep(0.025)
+
+	print('[*] END')
+	print('=============================================')
